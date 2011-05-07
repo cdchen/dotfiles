3fe84385c99c54cb32264a250e3a9ac59fa44104
Gregor Müllegger <gregor@muellegger.de>
2011-05-07 14:23:36 +0200

    git config and install fixes.

--- install.sh
+++ install.sh
@@ -8,8 +8,11 @@ ln -s `abspath zsh/zshrc.local` ~/.zshrc.local
 ln -s `abspath vim` ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
 
-ln -s `abspath vimperator/vimperatorrc` ~/.vimperatorrc
-ln -s `abspath vimperator/vimperatorrc.local` ~/.vimperatorrc.local
+ln -s `abspath gitignore` ~/.gitignore
+ln -s `abspath gitconfig` ~/.gitconfig
 
-mkdir ~/.vim/sessions
+ln -s `abspath pentadactyl/pentadactylrc` ~/.pentadactyl
+ln -s `abspath pentadactyl/pentadactylrc.local` ~/.pentadactyl.local
+
+test ! -e ~/.vim/sessions && mkdir ~/.vim/sessions
 touch ~/.vim_spinner_mru_files
